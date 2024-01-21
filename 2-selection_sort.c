d9c058fbb58d6679389365c1957cb583253dde8a
jordiindahood <hazem.ouni123@gmail.com>
2024-01-21 15:42:28 +0100

    script 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -27,7 +27,7 @@ void selection_sort(int *array, size_t size)
 			temp = array[i];
 			array[i] = array[min];
 			array[min] = temp;
+			print_array(array, size);
 		}
-		print_array(array, size);
 	}
 }
